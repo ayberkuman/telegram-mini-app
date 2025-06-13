efce129eaea01e0a00d42569d9f09cbabd37971b
Ayberk Uman <ayberkuman1@gmail.com>
2025-06-14 00:39:07 +0300

    refactor: reorganize imports in main.tsx for improved readability and maintainability

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-import { ApolloProvider } from "@apollo/client";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import "./i18n/i18n";
-import { TonConnectUIProvider } from "@tonconnect/ui-react";
-import { client } from "./apollo-client.ts";
 import { Provider as ChakraProvider } from "@/components/ui/provider";
 import { LanguageProvider } from "@/context/language-context";
 import { TasksProvider } from "@/context/tasks-context.tsx";
+import { ApolloProvider } from "@apollo/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { client } from "./apollo-client.ts";
+import App from "./App.tsx";
+import "./i18n/i18n";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
